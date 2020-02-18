ae72bd2155ebcaf5bcd67cec92cd5f287b6a7292
adibas03 <adibas03@ymail.com>
2020-02-18 02:30:46 +0100

    test

--- scripts/deploy/oz.initialize.js
+++ scripts/deploy/oz.initialize.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
 
-const { constants } = require('openzeppelin-test-helpers');
-
 const App = artifacts.require('App');
 const Router = artifacts.require('Router');
 
 const ENABLE_ROUTER_PACKAGE = '@enabledao/enable-router-contracts';
+const { constants } = require('openzeppelin-test-helpers');
 const { appCreate, getOZNetworkConfigByName } = require('../../test/helper');
 
 function activeNetwork() {
@@ -14,6 +13,15 @@ function activeNetwork() {
     return 'development';
   }
   return process.argv[networkIndex + 1];
+}
+
+function givenPauser(accounts) {
+  const pauserIndex = process.argv.lastIndexOf('--pauser');
+  if (pauserIndex < 2) {
+    return constants.ZERO_ADDRESS;
+  }
+  const pauser = process.argv[pauserIndex + 1];
+  return +pauser == pauser ? accounts[pauser] : pauser;
 }
 
 function activeNetworkName() {
@@ -28,14 +36,16 @@ function getFactory(contract) {
 
 async function initializeRouter(routerAddress, pauser) {
   const router = await Router.at(routerAddress);
-  return router.initialize();
+  return router.initialize(pauser);
 }
 
 module.exports = async () => {
   try {
+    const accounts = await web3.eth.getAccounts();
     const routerAddress = getFactory('Router').address;
     console.log('Router to initialize:', routerAddress);
-    const initializeTx = await initializeRouter(routerAddress);
+    console.log('pauser:', givenPauser(accounts));
+    const initializeTx = await initializeRouter(routerAddress, givenPauser(accounts));
     console.log('initializeTx:', initializeTx.tx);
     console.log(initializeTx.receipt.status ? 'Success!!' : 'Failure...');
   } catch (e) {
---
+++ test/unit/pausable.test.js
@@ -0,0 +1,42 @@
+import { BN, constants, expectEvent, expectRevert } from 'openzeppelin-test-helpers';
+
+const { expect } = require('chai');
+
+const Pausable = artifacts.require('Router');
+
+const getLastBlockTime = async () => {
+  await time.advanceBlock();
+  return await time.latest();
+};
+
+contract('Pausable', accounts => {
+  let pausable;
+
+  beforeEach(async () => {
+    pausable = await Pausable.new();
+    await pausable.initialize(accounts[0]);
+  });
+
+  it('Pausable should deploy successfully', async () => {
+    assert.exists(pausable.address, 'Pausable was not successfully deployed');
+  });
+
+  it('should pause successfully', async () => {
+    const receipt = await pausable.pause();
+    await expectEvent(receipt, 'Paused', { account: accounts[0] });
+    await expectRevert(
+      pausable.routeFunds(
+        constants.ZERO_ADDRESS,
+        1,
+        [accounts[1]],
+        [
+          new BN(10)
+            .pow(new BN(18))
+            .mul(new BN(5))
+            .div(new BN(100))
+        ]
+      ),
+      'Pausable: paused'
+    );
+  });
+});
